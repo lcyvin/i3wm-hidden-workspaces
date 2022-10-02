48b1a2b59ea6929820ad08bf49120a8e269aa480
Totooria <lucy@lucyvin.com>
2022-10-01 22:50:37 -0400

    add store function

--- main.go
+++ main.go
@@ -13,6 +13,13 @@ import (
 )
 
 type i3Cmd string
+type layoutType string
+
+type Result struct {
+	Err  error
+	Msg  string
+	Data interface{}
+}
 
 type KVEntry struct {
 	Key   string
@@ -64,12 +71,24 @@ func (kv *KVEntry) Fetch(db *badger.DB) error {
 	return err
 }
 
+// we'll do floating later absolutely fuck that for now
+const (
+	splith  layoutType = "splith"
+	splitv             = "splitv"
+	stacked            = "stacked"
+	tabbed             = "tabbed"
+)
+
 const (
 	mark      i3Cmd = "mark"
 	hide            = "move scratchpad"
 	show            = "scratchpad show"
 	focus           = "focus"
 	workspace       = "workspace"
+	resize          = "resize"
+	toggle          = "toggle"
+	save            = "save" // this and load are meta-instructions for i3wm-hidden-workspaces
+	load            = "load" // i3msg doesn't know what these mean in the context we need
 )
 
 type i3Instruction struct {
@@ -77,6 +96,18 @@ type i3Instruction struct {
 	CMD       []i3Cmd
 	Workspace string
 	Marks     []MarkID
+	Layout    []layoutInstruction
+}
+
+type Rect struct {
+	Height int64 `json:"height"`
+	Width  int64 `json:"width"`
+}
+
+type layoutInstruction struct {
+	Mark string
+	Type layoutType `json:"type"`
+	Size Rect       `json:"dimensions"`
 }
 
 type MarkID struct {
@@ -139,6 +170,33 @@ func getChildIDs(node *i3.Node) []string {
 	return ids
 }
 
+func getLayoutInstructions(node *i3.Node) []layoutInstruction {
+	inst := make([]layoutInstruction, 0)
+
+	// I do not have the brain power to do complex stacked/tabbed layouts at the moment
+	if node.Layout == "tabbed" {
+		inst = append(inst, layoutInstruction{Type: tabbed})
+		return inst
+	} else if node.Layout == "stacked" {
+		inst = append(inst, layoutInstruction{Type: stacked})
+		return inst
+	}
+	for _, n := range node.Nodes {
+		if len(n.Nodes) > 0 {
+			// recurse!
+			inst = append(inst, getLayoutInstructions(n)...)
+		} else {
+			inst = append(inst, layoutInstruction{
+				Type: layoutType(n.Layout),
+				Mark: n.Marks[0],
+				Size: Rect{n.Rect.Height, n.Rect.Width},
+			})
+		}
+	}
+
+	return inst
+}
+
 func focusMonitor(cmdData chan<- i3Instruction) {
 	recv := i3.Subscribe(i3.WorkspaceEventType)
 	defer recv.Close()
@@ -157,9 +215,10 @@ func focusMonitor(cmdData chan<- i3Instruction) {
 				}
 				cmd := i3Instruction{
 					Data:      "store",
-					CMD:       []i3Cmd{mark, hide},
+					CMD:       []i3Cmd{mark, save, hide},
 					Workspace: evt.Old.Name,
 					Marks:     marks,
+					Layout:    getLayoutInstructions(&evt.Old),
 				}
 				cmdData <- cmd
 			}
@@ -168,12 +227,50 @@ func focusMonitor(cmdData chan<- i3Instruction) {
 				cmd := i3Instruction{
 					Data:      "fetch",
 					Workspace: evt.Current.Name,
-					CMD:       []i3Cmd{focus},
+					CMD:       []i3Cmd{focus, load},
 				}
 				cmdData <- cmd
 			}
 		default:
 		}
+	}
+}
+
+func handleWatcherMsg(msg i3Instruction, db *badger.DB, result chan<- Result) {
+	switch msg.Data {
+	case "store":
+		res := Result{}
+		marks, _ := json.Marshal(msg.Marks)
+		kve := New("workspace-"+msg.Workspace, marks)
+		err := kve.Store(db)
+		if err != nil {
+			res.Err = err
+			result <- res
+			return
+		}
+
+		res.Msg = fmt.Sprintf("Stored key: workspace-%s, value: %s\n", msg.Workspace, marks)
+
+		layout, _ := json.Marshal(msg.Layout)
+		kve = New("layout-"+msg.Workspace, layout)
+		err = kve.Store(db)
+		if err != nil {
+			res.Err = err
+			result <- res
+			return
+		}
+
+		res.Msg = res.Msg + fmt.Sprintf("Stored key: layout-%s, value: %s\n", msg.Workspace, layout)
+
+		err = msg.Run()
+		if err != nil {
+			res.Err = err
+			result <- res
+			return
+		}
+
+	case "fetch":
+
 	}
 }
 
@@ -184,45 +281,19 @@ func main() {
 	}
 	defer db.Close()
 
+	result := make(chan Result)
 	cmdData := make(chan i3Instruction)
 	go focusMonitor(cmdData)
 	for {
 		select {
 		case msg := <-cmdData:
-			if msg.Data == "store" {
-				marks, _ := json.Marshal(msg.Marks)
-				err := db.Update(func(txn *badger.Txn) error {
-					err := txn.Set([]byte(msg.Workspace), marks)
-					return err
-				})
+			go handleWatcherMsg(msg, db, result)
+		case res := <-result:
+			if res.Err != nil {
 				fmt.Println(err)
-				err = msg.Run()
-				if err != nil {
-					fmt.Println(err)
-				}
-			} else if msg.Data == "fetch" {
-				err := db.View(func(txn *badger.Txn) error {
-					item, err := txn.Get([]byte(msg.Workspace))
-					if err != nil {
-						fmt.Println(err)
-						return err
-					}
-
-					item.Value(func(val []byte) error {
-						marks := make([]MarkID, 0)
-						json.Unmarshal(val, &marks)
-						msg.Marks = marks
-						return nil
-					})
-					return nil
-				})
-				if err != nil {
-					fmt.Println(err)
-				}
-				err = msg.Run()
-				if err != nil {
-					fmt.Println(err)
-				}
+			}
+			if res.Msg != "" {
+				fmt.Println(res.Msg)
 			}
 		}
 	}
